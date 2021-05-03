0ede70955fd3f8ec9966540c3730db23db676796
Sidney Arcidiacono <sidneyarci@gmail.com>
2021-05-03 13:41:29 -0700

    Completed all TODOs, all tests passing.

--- books_app/auth/tests.py
+++ books_app/auth/tests.py
@@ -2,7 +2,7 @@ import os
 from unittest import TestCase
 
 from datetime import date
- 
+
 from books_app import app, db, bcrypt
 from books_app.models import Book, Author, User, Audience
 
@@ -15,83 +15,135 @@ python -m unittest books_app.main.tests
 # Setup
 #################################################
 
+
 def create_books():
-    a1 = Author(name='Harper Lee')
+    a1 = Author(name="Harper Lee")
     b1 = Book(
-        title='To Kill a Mockingbird',
+        title="To Kill a Mockingbird",
         publish_date=date(1960, 7, 11),
-        author=a1
+        author=a1,
     )
     db.session.add(b1)
 
-    a2 = Author(name='Sylvia Plath')
-    b2 = Book(title='The Bell Jar', author=a2)
+    a2 = Author(name="Sylvia Plath")
+    b2 = Book(title="The Bell Jar", author=a2)
     db.session.add(b2)
     db.session.commit()
 
+
 def create_user():
-    password_hash = bcrypt.generate_password_hash('password').decode('utf-8')
-    user = User(username='me1', password=password_hash)
+    password_hash = bcrypt.generate_password_hash("password").decode("utf-8")
+    user = User(username="me1", password=password_hash)
     db.session.add(user)
     db.session.commit()
+
 
 #################################################
 # Tests
 #################################################
 
+
 class AuthTests(TestCase):
     """Tests for authentication (login & signup)."""
- 
+
     def setUp(self):
         """Executed prior to each test."""
-        app.config['TESTING'] = True
-        app.config['WTF_CSRF_ENABLED'] = False
-        app.config['DEBUG'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+        app.config["TESTING"] = True
+        app.config["WTF_CSRF_ENABLED"] = False
+        app.config["DEBUG"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
         self.app = app.test_client()
         db.drop_all()
         db.create_all()
 
     def test_signup(self):
-        # TODO: Write a test for the signup route. It should:
-        # - Make a POST request to /signup, sending a username & password
-        # - Check that the user now exists in the database
-        pass
+        """Test signup."""
+        post_data = {
+            "username": "Sid",
+            "password": "password",
+        }
+        self.app.post("/signup", data=post_data)
+
+        created_user = User.query.filter_by(username="Sid").one()
+        self.assertIsNotNone(created_user)
+        self.assertEqual(created_user.username, "Sid")
 
     def test_signup_existing_user(self):
-        # TODO: Write a test for the signup route. It should:
-        # - Create a user
-        # - Make a POST request to /signup, sending the same username & password
-        # - Check that the form is displayed again with an error message
-        pass
+        """Test that signing up an existing user won't work."""
+        create_user()
+
+        post_data = {
+            "username": "me1",
+            "password": "password",
+        }
+        response = self.app.post("/signup", data=post_data)
+
+        response_text = response.get_data(as_text=True)
+        self.assertIn("Sign Up", response_text)
+        self.assertIn(
+            "That username is taken. Please choose a different one.",
+            response_text,
+        )
 
     def test_login_correct_password(self):
-        # TODO: Write a test for the login route. It should:
-        # - Create a user
-        # - Make a POST request to /login, sending the created username & password
-        # - Check that the "login" button is not displayed on the homepage
-        pass
+        """Test logging in with the correct password."""
+        post_data = {
+            "username": "me1",
+            "password": "password",
+        }
+        self.app.post("/signup", data=post_data)
+
+        response = self.app.post("/login", data=post_data)
+
+        response_text = response.get_data(as_text=True)
+        self.assertIn("Redirecting...", response_text)
+        self.assertNotIn("Log In", response_text)
 
     def test_login_nonexistent_user(self):
-        # TODO: Write a test for the login route. It should:
-        # - Make a POST request to /login, sending a username & password
-        # - Check that the login form is displayed again, with an appropriate
-        #   error message
-        pass
+        """Test that logging in a nonexistent user fails."""
+        post_data = {
+            "username": "me1",
+            "password": "password",
+        }
+        response = self.app.post("/login", data=post_data)
+
+        response_text = response.get_data(as_text=True)
+        self.assertIn("Log In", response_text)
+        self.assertIn(
+            "No user with that username. Please try again.", response_text
+        )
+        self.assertNotIn("Log Out", response_text)
 
     def test_login_incorrect_password(self):
-        # TODO: Write a test for the login route. It should:
-        # - Create a user
-        # - Make a POST request to /login, sending the created username &
-        #   an incorrect password
-        # - Check that the login form is displayed again, with an appropriate
-        #   error message
-        pass
+        """Ensure that a user cannot log in with the incorrect password."""
+        create_user()
+
+        post_data = {
+            "username": "me1",
+            "password": "passwords",
+        }
+        response = self.app.post("/login", data=post_data)
+
+        response_text = response.get_data(as_text=True)
+        self.assertIn("Log In", response_text)
+        self.assertIn(
+            "Password doesn&#39;t match. Please try again", response_text
+        )
+        self.assertNotIn("Log Out", response_text)
 
     def test_logout(self):
-        # TODO: Write a test for the logout route. It should:
-        # - Create a user
-        # - Log the user in (make a POST request to /login)
-        # - Make a GET request to /logout
-        # - Check that the "login" button appears on the homepage
-        pass
+        """Test logout."""
+        create_user()
+
+        post_data = {
+            "username": "me1",
+            "password": "password",
+        }
+        self.app.post("/login", data=post_data)
+
+        response = self.app.get("/logout", follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+        response_text = response.get_data(as_text=True)
+        self.assertIn("Log In", response_text)
+        self.assertNotIn("Log Out", response_text)
--- books_app/main/tests.py
+++ books_app/main/tests.py
@@ -2,9 +2,9 @@ import os
 import unittest
 
 from datetime import date
- 
+
 from books_app import app, db, bcrypt
-from books_app.models import Book, Author, User, Audience
+from books_app.models import Book, Author, User, Audience, Genre
 
 """
 Run these tests with the command:
@@ -15,52 +15,58 @@ python -m unittest books_app.main.tests
 # Setup
 #################################################
 
+
 def login(client, username, password):
-    return client.post('/login', data=dict(
-        username=username,
-        password=password
-    ), follow_redirects=True)
+    return client.post(
+        "/login",
+        data=dict(username=username, password=password),
+        follow_redirects=True,
+    )
+
 
 def logout(client):
-    return client.get('/logout', follow_redirects=True)
+    return client.get("/logout", follow_redirects=True)
+
 
 def create_books():
-    a1 = Author(name='Harper Lee')
+    a1 = Author(name="Harper Lee")
     b1 = Book(
-        title='To Kill a Mockingbird',
+        title="To Kill a Mockingbird",
         publish_date=date(1960, 7, 11),
-        author=a1
+        author=a1,
     )
     db.session.add(b1)
 
-    a2 = Author(name='Sylvia Plath')
-    b2 = Book(title='The Bell Jar', author=a2)
+    a2 = Author(name="Sylvia Plath")
+    b2 = Book(title="The Bell Jar", author=a2)
     db.session.add(b2)
     db.session.commit()
 
+
 def create_user():
     # Creates a user with username 'me1' and password of 'password'
-    password_hash = bcrypt.generate_password_hash('password').decode('utf-8')
-    user = User(username='me1', password=password_hash)
+    password_hash = bcrypt.generate_password_hash("password").decode("utf-8")
+    user = User(username="me1", password=password_hash)
     db.session.add(user)
     db.session.commit()
+
 
 #################################################
 # Tests
 #################################################
 
+
 class MainTests(unittest.TestCase):
- 
     def setUp(self):
         """Executed prior to each test."""
-        app.config['TESTING'] = True
-        app.config['WTF_CSRF_ENABLED'] = False
-        app.config['DEBUG'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+        app.config["TESTING"] = True
+        app.config["WTF_CSRF_ENABLED"] = False
+        app.config["DEBUG"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
         self.app = app.test_client()
         db.drop_all()
         db.create_all()
- 
+
     def test_homepage_logged_out(self):
         """Test that the books show up on the homepage."""
         # Set up
@@ -68,95 +74,109 @@ class MainTests(unittest.TestCase):
         create_user()
 
         # Make a GET request
-        response = self.app.get('/', follow_redirects=True)
+        response = self.app.get("/", follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
         # Check that page contains all of the things we expect
         response_text = response.get_data(as_text=True)
-        self.assertIn('To Kill a Mockingbird', response_text)
-        self.assertIn('The Bell Jar', response_text)
-        self.assertIn('me1', response_text)
-        self.assertIn('Log In', response_text)
-        self.assertIn('Sign Up', response_text)
+        self.assertIn("To Kill a Mockingbird", response_text)
+        self.assertIn("The Bell Jar", response_text)
+        self.assertIn("me1", response_text)
+        self.assertIn("Log In", response_text)
+        self.assertIn("Sign Up", response_text)
 
         # Check that the page doesn't contain things we don't expect
         # (these should be shown only to logged in users)
-        self.assertNotIn('Create Book', response_text)
-        self.assertNotIn('Create Author', response_text)
-        self.assertNotIn('Create Genre', response_text)
- 
+        self.assertNotIn("Create Book", response_text)
+        self.assertNotIn("Create Author", response_text)
+        self.assertNotIn("Create Genre", response_text)
+
     def test_homepage_logged_in(self):
         """Test that the books show up on the homepage."""
         # Set up
         create_books()
         create_user()
-        login(self.app, 'me1', 'password')
+        login(self.app, "me1", "password")
 
         # Make a GET request
-        response = self.app.get('/', follow_redirects=True)
+        response = self.app.get("/", follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
         # Check that page contains all of the things we expect
         response_text = response.get_data(as_text=True)
-        self.assertIn('To Kill a Mockingbird', response_text)
-        self.assertIn('The Bell Jar', response_text)
-        self.assertIn('me1', response_text)
-        self.assertIn('Create Book', response_text)
-        self.assertIn('Create Author', response_text)
-        self.assertIn('Create Genre', response_text)
+        self.assertIn("To Kill a Mockingbird", response_text)
+        self.assertIn("The Bell Jar", response_text)
+        self.assertIn("me1", response_text)
+        self.assertIn("Create Book", response_text)
+        self.assertIn("Create Author", response_text)
+        self.assertIn("Create Genre", response_text)
 
         # Check that the page doesn't contain things we don't expect
         # (these should be shown only to logged out users)
-        self.assertNotIn('Log In', response_text)
-        self.assertNotIn('Sign Up', response_text)
+        self.assertNotIn("Log In", response_text)
+        self.assertNotIn("Sign Up", response_text)
 
     def test_book_detail_logged_out(self):
         """Test that the book appears on its detail page."""
-        # TODO: Use helper functions to create books, authors, user
+        create_books()
+        create_user()
 
-        # TODO: Make a GET request to the URL /book/1, check to see that the
-        # status code is 200
+        response = self.app.get("/book/1", follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
 
-        # TODO: Check that the response contains the book's title, publish date,
-        # and author's name
+        response_text = response.get_data(as_text=True)
+        self.assertIn("To Kill a Mockingbird", response_text)
+        self.assertIn("Harper Lee", response_text)
+        self.assertIn("July 11, 1960", response_text)
+        self.assertIn("Log In", response_text)
+        self.assertIn("Sign Up", response_text)
 
-        # TODO: Check that the response does NOT contain the 'Favorite' button
-        # (it should only be shown to logged in users)
-        pass
+        self.assertNotIn("Log Out", response_text)
+        self.assertNotIn("Favorite This Book", response_text)
 
     def test_book_detail_logged_in(self):
         """Test that the book appears on its detail page."""
-        # TODO: Use helper functions to create books, authors, user, & to log in
+        create_books()
+        create_user()
+        login(self.app, "me1", "password")
 
-        # TODO: Make a GET request to the URL /book/1, check to see that the
-        # status code is 200
+        response = self.app.get("/book/1", follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
 
-        # TODO: Check that the response contains the book's title, publish date,
-        # and author's name
+        response_text = response.get_data(as_text=True)
+        self.assertIn("To Kill a Mockingbird", response_text)
+        self.assertIn("Harper Lee", response_text)
+        self.assertIn("July 11, 1960", response_text)
+        self.assertIn("me1", response_text)
+        self.assertIn("Create Book", response_text)
+        self.assertIn("Create Author", response_text)
+        self.assertIn("Create Genre", response_text)
+        self.assertIn("Favorite This Book", response_text)
+        self.assertIn("Log Out", response_text)
 
-        # TODO: Check that the response contains the 'Favorite' button
-        pass
+        self.assertNotIn("Log In", response_text)
+        self.assertNotIn("Sign Up", response_text)
 
     def test_update_book(self):
         """Test updating a book."""
         # Set up
         create_books()
         create_user()
-        login(self.app, 'me1', 'password')
+        login(self.app, "me1", "password")
 
         # Make POST request with data
         post_data = {
-            'title': 'Tequila Mockingbird',
-            'publish_date': '1960-07-12',
-            'author': 1,
-            'audience': 'CHILDREN',
-            'genres': []
+            "title": "Tequila Mockingbird",
+            "publish_date": "1960-07-12",
+            "author": 1,
+            "audience": "CHILDREN",
+            "genres": [],
         }
-        self.app.post('/book/1', data=post_data)
-        
+        self.app.post("/book/1", data=post_data)
+
         # Make sure the book was updated as we'd expect
         book = Book.query.get(1)
-        self.assertEqual(book.title, 'Tequila Mockingbird')
+        self.assertEqual(book.title, "Tequila Mockingbird")
         self.assertEqual(book.publish_date, date(1960, 7, 12))
         self.assertEqual(book.audience, Audience.CHILDREN)
 
@@ -165,26 +185,26 @@ class MainTests(unittest.TestCase):
         # Set up
         create_books()
         create_user()
-        login(self.app, 'me1', 'password')
+        login(self.app, "me1", "password")
 
         # Make POST request with data
         post_data = {
-            'title': 'Go Set a Watchman',
-            'publish_date': '2015-07-14',
-            'author': 1,
-            'audience': 'ADULT',
-            'genres': []
+            "title": "Go Set a Watchman",
+            "publish_date": "2015-07-14",
+            "author": 1,
+            "audience": "ADULT",
+            "genres": [],
         }
-        self.app.post('/create_book', data=post_data)
+        self.app.post("/create_book", data=post_data)
 
         # Make sure book was updated as we'd expect
-        created_book = Book.query.filter_by(title='Go Set a Watchman').one()
+        created_book = Book.query.filter_by(title="Go Set a Watchman").one()
         self.assertIsNotNone(created_book)
-        self.assertEqual(created_book.author.name, 'Harper Lee')
+        self.assertEqual(created_book.author.name, "Harper Lee")
 
     def test_create_book_logged_out(self):
         """
-        Test that the user is redirected when trying to access the create book 
+        Test that the user is redirected when trying to access the create book
         route if not logged in.
         """
         # Set up
@@ -192,44 +212,86 @@ class MainTests(unittest.TestCase):
         create_user()
 
         # Make GET request
-        response = self.app.get('/create_book')
+        response = self.app.get("/create_book")
 
         # Make sure that the user was redirecte to the login page
         self.assertEqual(response.status_code, 302)
-        self.assertIn('/login?next=%2Fcreate_book', response.location)
+        self.assertIn("/login?next=%2Fcreate_book", response.location)
 
     def test_create_author(self):
         """Test creating an author."""
-        # TODO: Make a POST request to the /create_author route
+        create_books()
+        create_user()
+        login(self.app, "me1", "password")
 
-        # TODO: Verify that the author was updated in the database
-        pass
+        post_data = {
+            "name": "Dostoyevsky",
+            "biography": "Russian biography stuff.",
+        }
+        self.app.post("/create_author", data=post_data)
+
+        created_author = Author.query.filter_by(name="Dostoyevsky").one()
+        self.assertIsNotNone(created_author)
+        self.assertEqual(created_author.name, "Dostoyevsky")
+        self.assertEqual(created_author.biography, "Russian biography stuff.")
 
     def test_create_genre(self):
-        # TODO: Make a POST request to the /create_genre route, 
+        """Test creating a genre."""
+        create_books()
+        create_user()
+        login(self.app, "me1", "password")
 
-        # TODO: Verify that the genre was updated in the database
-        pass
+        post_data = {
+            "name": "Philosophy",
+        }
+        self.app.post("/create_genre", data=post_data)
+
+        created_genre = Genre.query.filter_by(name="Philosophy").one()
+        self.assertIsNotNone(created_genre)
+        self.assertEqual(created_genre.name, "Philosophy")
 
     def test_profile_page(self):
-        # TODO: Make a GET request to the /profile/1 route
+        create_books()
+        create_user()
+        login(self.app, "me1", "password")
 
-        # TODO: Verify that the response shows the appropriate user info
-        pass
+        response = self.app.get("/profile/me1", follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+        response_text = response.get_data(as_text=True)
+        self.assertIn("me1", response_text)
+        self.assertIn("Create Book", response_text)
+        self.assertIn("Create Author", response_text)
+        self.assertIn("Create Genre", response_text)
+        self.assertIn("Log Out", response_text)
+
+        self.assertNotIn("Log In", response_text)
+        self.assertNotIn("Sign Up", response_text)
 
     def test_favorite_book(self):
-        # TODO: Login as the user me1
+        create_books()
+        create_user()
+        login(self.app, "me1", "password")
 
-        # TODO: Make a POST request to the /favorite/1 route
+        self.app.post("/favorite/1")
 
-        # TODO: Verify that the book with id 1 was added to the user's favorites
-        pass
+        user = User.query.filter_by(username="me1").one()
+        self.assertIsNotNone(user)
+        self.assertEqual(user.username, "me1")
+        self.assertEqual(
+            user.favorite_books[0].title, "To Kill a Mockingbird"
+        )
 
     def test_unfavorite_book(self):
-        # TODO: Login as the user me1, and add book with id 1 to me1's favorites
+        create_books()
+        create_user()
+        login(self.app, "me1", "password")
 
-        # TODO: Make a POST request to the /unfavorite/1 route
+        self.app.post("/favorite/1")
 
-        # TODO: Verify that the book with id 1 was removed from the user's 
-        # favorites
-        pass
+        self.app.post("/unfavorite/1")
+
+        user = User.query.filter_by(username="me1").one()
+        self.assertIsNotNone(user)
+        self.assertEqual(user.username, "me1")
+        self.assertEqual(user.favorite_books, [])
